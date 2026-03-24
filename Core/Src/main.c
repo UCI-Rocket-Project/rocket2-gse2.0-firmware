fec04305cab79a4ff2a7d7f447f2d070a8dfff84
Robert Woo <RobertAWoo714@github.com>
2026-03-24 04:41:53 -0700

    GSE v2.1: Updated IOC file for GSEv2.1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -42,10 +42,9 @@
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
 
-SPI_HandleTypeDef hspi3;
+I2C_HandleTypeDef hi2c1;
 
-TIM_HandleTypeDef htim4;
-TIM_HandleTypeDef htim5;
+SPI_HandleTypeDef hspi3;
 
 UART_HandleTypeDef huart3;
 
@@ -62,8 +61,7 @@ static void MX_ADC1_Init(void);
 static void MX_SPI3_Init(void);
 static void MX_USART3_UART_Init(void);
 static void MX_USB_OTG_FS_PCD_Init(void);
-static void MX_TIM4_Init(void);
-static void MX_TIM5_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -106,8 +104,7 @@ int main(void)
   MX_SPI3_Init();
   MX_USART3_UART_Init();
   MX_USB_OTG_FS_PCD_Init();
-  MX_TIM4_Init();
-  MX_TIM5_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
 
   cpp_main();
@@ -139,8 +136,8 @@ void SystemClock_Config(void)
   * in the RCC_OscInitTypeDef structure.
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV2;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.Prediv1Source = RCC_PREDIV1_SOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -212,7 +209,7 @@ static void MX_ADC1_Init(void)
 
   /** Configure Regular Channel
   */
-  sConfig.Channel = ADC_CHANNEL_0;
+  sConfig.Channel = ADC_CHANNEL_10;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
@@ -222,6 +219,40 @@ static void MX_ADC1_Init(void)
   /* USER CODE BEGIN ADC1_Init 2 */
 
   /* USER CODE END ADC1_Init 2 */
+
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 100000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
 
 }
 
@@ -248,7 +279,7 @@ static void MX_SPI3_Init(void)
   hspi3.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi3.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi3.Init.NSS = SPI_NSS_SOFT;
-  hspi3.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
+  hspi3.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
   hspi3.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi3.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi3.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
@@ -260,97 +291,6 @@ static void MX_SPI3_Init(void)
   /* USER CODE BEGIN SPI3_Init 2 */
 
   /* USER CODE END SPI3_Init 2 */
-
-}
-
-/**
-  * @brief TIM4 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM4_Init(void)
-{
-
-  /* USER CODE BEGIN TIM4_Init 0 */
-
-  /* USER CODE END TIM4_Init 0 */
-
-  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-  /* USER CODE BEGIN TIM4_Init 1 */
-
-  /* USER CODE END TIM4_Init 1 */
-  htim4.Instance = TIM4;
-  htim4.Init.Prescaler = 36000;
-  htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim4.Init.Period = 65535;
-  htim4.Init.ClockDivision = TIM_CLOCKDIVISION_DIV2;
-  htim4.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim4) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-  if (HAL_TIM_ConfigClockSource(&htim4, &sClockSourceConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim4, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM4_Init 2 */
-
-  /* USER CODE END TIM4_Init 2 */
-
-}
-
-/**
-  * @brief TIM5 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_TIM5_Init(void)
-{
-
-  /* USER CODE BEGIN TIM5_Init 0 */
-
-  /* USER CODE END TIM5_Init 0 */
-
-  TIM_SlaveConfigTypeDef sSlaveConfig = {0};
-  TIM_MasterConfigTypeDef sMasterConfig = {0};
-
-  /* USER CODE BEGIN TIM5_Init 1 */
-
-  /* USER CODE END TIM5_Init 1 */
-  htim5.Instance = TIM5;
-  htim5.Init.Prescaler = 0;
-  htim5.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim5.Init.Period = 65535;
-  htim5.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
-  htim5.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
-  if (HAL_TIM_Base_Init(&htim5) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sSlaveConfig.SlaveMode = TIM_SLAVEMODE_EXTERNAL1;
-  sSlaveConfig.InputTrigger = TIM_TS_ITR2;
-  if (HAL_TIM_SlaveConfigSynchro(&htim5, &sSlaveConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
-  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-  if (HAL_TIMEx_MasterConfigSynchronization(&htim5, &sMasterConfig) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN TIM5_Init 2 */
-
-  /* USER CODE END TIM5_Init 2 */
 
 }
 
@@ -375,7 +315,7 @@ static void MX_USART3_UART_Init(void)
   huart3.Init.StopBits = UART_STOPBITS_1;
   huart3.Init.Parity = UART_PARITY_NONE;
   huart3.Init.Mode = UART_MODE_TX_RX;
-  huart3.Init.HwFlowCtl = UART_HWCONTROL_RTS_CTS;
+  huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
   huart3.Init.OverSampling = UART_OVERSAMPLING_16;
   if (HAL_UART_Init(&huart3) != HAL_OK)
   {
@@ -438,45 +378,42 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOC, SOLENOID6_EN_Pin|SOLENOID7_EN_Pin|SOLENOID8_EN_Pin|EMATCH0_FIRE_Pin
-                          |EMATCH1_FIRE_Pin|TC3_nCS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, SOLENOID2_EN_Pin|SOLENOID1_EN_Pin|SOLENOID0_EN_Pin|SOLENOID10_EN_Pin
+                          |SOLENOID11_EN_Pin|EMATCH0_FIRE_Pin|EMATCH1_FIRE_Pin|SOLENOID9_EN_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, ETH_nRST_Pin|ALARM_Pin|TC5_nCS_Pin|SOLENOID0_EN_Pin
-                          |SOLENOID1_EN_Pin|SOLENOID2_EN_Pin|SOLENOID3_EN_Pin|SOLENOID4_EN_Pin
-                          |SOLENOID5_EN_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, ETH_RST_Pin|ALARM_Pin|ETH_RTS_Pin|SOLENOID7_EN_Pin
+                          |SOLENOID6_EN_Pin|SOLENOID5_EN_Pin|SOLENOID4_EN_Pin|SOLENOID3_EN_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, TC0_nCS_Pin|TC1_nCS_Pin|TC2_nCS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, TC0_CS_Pin|TC1_CS_Pin|TC2_CS_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(TC4_nCS_GPIO_Port, TC4_nCS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(SOLENOID8_EN_GPIO_Port, SOLENOID8_EN_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : SOLENOID6_EN_Pin SOLENOID7_EN_Pin SOLENOID8_EN_Pin EMATCH0_FIRE_Pin
-                           EMATCH1_FIRE_Pin TC3_nCS_Pin */
-  GPIO_InitStruct.Pin = SOLENOID6_EN_Pin|SOLENOID7_EN_Pin|SOLENOID8_EN_Pin|EMATCH0_FIRE_Pin
-                          |EMATCH1_FIRE_Pin|TC3_nCS_Pin;
+  /*Configure GPIO pins : SOLENOID2_EN_Pin SOLENOID1_EN_Pin SOLENOID0_EN_Pin SOLENOID10_EN_Pin
+                           SOLENOID11_EN_Pin EMATCH0_FIRE_Pin EMATCH1_FIRE_Pin SOLENOID9_EN_Pin */
+  GPIO_InitStruct.Pin = SOLENOID2_EN_Pin|SOLENOID1_EN_Pin|SOLENOID0_EN_Pin|SOLENOID10_EN_Pin
+                          |SOLENOID11_EN_Pin|EMATCH0_FIRE_Pin|EMATCH1_FIRE_Pin|SOLENOID9_EN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : ETH_nRST_Pin ALARM_Pin TC5_nCS_Pin SOLENOID0_EN_Pin
-                           SOLENOID1_EN_Pin SOLENOID2_EN_Pin SOLENOID3_EN_Pin SOLENOID4_EN_Pin
-                           SOLENOID5_EN_Pin */
-  GPIO_InitStruct.Pin = ETH_nRST_Pin|ALARM_Pin|TC5_nCS_Pin|SOLENOID0_EN_Pin
-                          |SOLENOID1_EN_Pin|SOLENOID2_EN_Pin|SOLENOID3_EN_Pin|SOLENOID4_EN_Pin
-                          |SOLENOID5_EN_Pin;
+  /*Configure GPIO pins : ETH_RST_Pin ALARM_Pin ETH_RTS_Pin SOLENOID7_EN_Pin
+                           SOLENOID6_EN_Pin SOLENOID5_EN_Pin SOLENOID4_EN_Pin SOLENOID3_EN_Pin */
+  GPIO_InitStruct.Pin = ETH_RST_Pin|ALARM_Pin|ETH_RTS_Pin|SOLENOID7_EN_Pin
+                          |SOLENOID6_EN_Pin|SOLENOID5_EN_Pin|SOLENOID4_EN_Pin|SOLENOID3_EN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : ARMED_Pin */
-  GPIO_InitStruct.Pin = ARMED_Pin;
+  /*Configure GPIO pins : ETH_CTS_Pin ARMED_Pin */
+  GPIO_InitStruct.Pin = ETH_CTS_Pin|ARMED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(ARMED_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
   /*Configure GPIO pins : EMATCH0_CONT_Pin EMATCH1_CONT_Pin */
   GPIO_InitStruct.Pin = EMATCH0_CONT_Pin|EMATCH1_CONT_Pin;
@@ -484,19 +421,19 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : TC0_nCS_Pin TC1_nCS_Pin TC2_nCS_Pin */
-  GPIO_InitStruct.Pin = TC0_nCS_Pin|TC1_nCS_Pin|TC2_nCS_Pin;
+  /*Configure GPIO pins : TC0_CS_Pin TC1_CS_Pin TC2_CS_Pin */
+  GPIO_InitStruct.Pin = TC0_CS_Pin|TC1_CS_Pin|TC2_CS_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : TC4_nCS_Pin */
-  GPIO_InitStruct.Pin = TC4_nCS_Pin;
+  /*Configure GPIO pin : SOLENOID8_EN_Pin */
+  GPIO_InitStruct.Pin = SOLENOID8_EN_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(TC4_nCS_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(SOLENOID8_EN_GPIO_Port, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
--- Core/Src/stm32f1xx_hal_msp.c
+++ Core/Src/stm32f1xx_hal_msp.c
@@ -17,7 +17,6 @@
   ******************************************************************************
   */
 /* USER CODE END Header */
-
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 /* USER CODE BEGIN Includes */
@@ -114,18 +113,15 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
     PA5     ------> ADC1_IN5
     PA6     ------> ADC1_IN6
     PA7     ------> ADC1_IN7
-    PC4     ------> ADC1_IN14
-    PC5     ------> ADC1_IN15
     PB0     ------> ADC1_IN8
     PB1     ------> ADC1_IN9
     */
-    GPIO_InitStruct.Pin = SOLENOID0_FB_Pin|SOLENOID1_FB_Pin|SOLENOID2_FB_Pin|SOLENOID3_FB_Pin
-                          |PT3_Pin|PT4_Pin;
+    GPIO_InitStruct.Pin = SOLENOID0_FB_Pin|SOLENOID1_FB_Pin|SOLENOID2_FB_Pin|SOLENOID3_FB_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
     HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
     GPIO_InitStruct.Pin = SOLENOID4_FB_Pin|SOLENOID5_FB_Pin|SOLENOID6_FB_Pin|SOLENOID7_FB_Pin
-                          |SOLENOID8_FB_Pin|PT0_Pin|PT1_Pin|PT2_Pin;
+                          |SOLENOID8_FB_Pin|SOLENOID9_FB_Pin|SOLENOID10_FB_Pin|SOLENOID11_FB_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
@@ -170,22 +166,85 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* hadc)
     PA5     ------> ADC1_IN5
     PA6     ------> ADC1_IN6
     PA7     ------> ADC1_IN7
-    PC4     ------> ADC1_IN14
-    PC5     ------> ADC1_IN15
     PB0     ------> ADC1_IN8
     PB1     ------> ADC1_IN9
     */
-    HAL_GPIO_DeInit(GPIOC, SOLENOID0_FB_Pin|SOLENOID1_FB_Pin|SOLENOID2_FB_Pin|SOLENOID3_FB_Pin
-                          |PT3_Pin|PT4_Pin);
+    HAL_GPIO_DeInit(GPIOC, SOLENOID0_FB_Pin|SOLENOID1_FB_Pin|SOLENOID2_FB_Pin|SOLENOID3_FB_Pin);
 
     HAL_GPIO_DeInit(GPIOA, SOLENOID4_FB_Pin|SOLENOID5_FB_Pin|SOLENOID6_FB_Pin|SOLENOID7_FB_Pin
-                          |SOLENOID8_FB_Pin|PT0_Pin|PT1_Pin|PT2_Pin);
+                          |SOLENOID8_FB_Pin|SOLENOID9_FB_Pin|SOLENOID10_FB_Pin|SOLENOID11_FB_Pin);
 
     HAL_GPIO_DeInit(GPIOB, PWR0_Pin|PWR1_Pin);
 
     /* USER CODE BEGIN ADC1_MspDeInit 1 */
 
     /* USER CODE END ADC1_MspDeInit 1 */
+  }
+
+}
+
+/**
+  * @brief I2C MSP Initialization
+  * This function configures the hardware resources used in this example
+  * @param hi2c: I2C handle pointer
+  * @retval None
+  */
+void HAL_I2C_MspInit(I2C_HandleTypeDef* hi2c)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  if(hi2c->Instance==I2C1)
+  {
+    /* USER CODE BEGIN I2C1_MspInit 0 */
+
+    /* USER CODE END I2C1_MspInit 0 */
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    /**I2C1 GPIO Configuration
+    PB6     ------> I2C1_SCL
+    PB7     ------> I2C1_SDA
+    */
+    GPIO_InitStruct.Pin = EXT_ADC_SCL_Pin|EXT_ADC_SDA_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_OD;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /* Peripheral clock enable */
+    __HAL_RCC_I2C1_CLK_ENABLE();
+    /* USER CODE BEGIN I2C1_MspInit 1 */
+
+    /* USER CODE END I2C1_MspInit 1 */
+
+  }
+
+}
+
+/**
+  * @brief I2C MSP De-Initialization
+  * This function freeze the hardware resources used in this example
+  * @param hi2c: I2C handle pointer
+  * @retval None
+  */
+void HAL_I2C_MspDeInit(I2C_HandleTypeDef* hi2c)
+{
+  if(hi2c->Instance==I2C1)
+  {
+    /* USER CODE BEGIN I2C1_MspDeInit 0 */
+
+    /* USER CODE END I2C1_MspDeInit 0 */
+    /* Peripheral clock disable */
+    __HAL_RCC_I2C1_CLK_DISABLE();
+
+    /**I2C1 GPIO Configuration
+    PB6     ------> I2C1_SCL
+    PB7     ------> I2C1_SDA
+    */
+    HAL_GPIO_DeInit(EXT_ADC_SCL_GPIO_Port, EXT_ADC_SCL_Pin);
+
+    HAL_GPIO_DeInit(EXT_ADC_SDA_GPIO_Port, EXT_ADC_SDA_Pin);
+
+    /* USER CODE BEGIN I2C1_MspDeInit 1 */
+
+    /* USER CODE END I2C1_MspDeInit 1 */
   }
 
 }
@@ -212,15 +271,15 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef* hspi)
     PC10     ------> SPI3_SCK
     PC11     ------> SPI3_MISO
     */
-    GPIO_InitStruct.Pin = TC_SCK_Pin;
+    GPIO_InitStruct.Pin = GPIO_PIN_10;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(TC_SCK_GPIO_Port, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = TC_MISO_Pin;
+    GPIO_InitStruct.Pin = GPIO_PIN_11;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(TC_MISO_GPIO_Port, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
     __HAL_AFIO_REMAP_SPI3_ENABLE();
 
@@ -252,77 +311,11 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi)
     PC10     ------> SPI3_SCK
     PC11     ------> SPI3_MISO
     */
-    HAL_GPIO_DeInit(GPIOC, TC_SCK_Pin|TC_MISO_Pin);
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_10|GPIO_PIN_11);
 
     /* USER CODE BEGIN SPI3_MspDeInit 1 */
 
     /* USER CODE END SPI3_MspDeInit 1 */
-  }
-
-}
-
-/**
-  * @brief TIM_Base MSP Initialization
-  * This function configures the hardware resources used in this example
-  * @param htim_base: TIM_Base handle pointer
-  * @retval None
-  */
-void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
-{
-  if(htim_base->Instance==TIM4)
-  {
-    /* USER CODE BEGIN TIM4_MspInit 0 */
-
-    /* USER CODE END TIM4_MspInit 0 */
-    /* Peripheral clock enable */
-    __HAL_RCC_TIM4_CLK_ENABLE();
-    /* USER CODE BEGIN TIM4_MspInit 1 */
-
-    /* USER CODE END TIM4_MspInit 1 */
-  }
-  else if(htim_base->Instance==TIM5)
-  {
-    /* USER CODE BEGIN TIM5_MspInit 0 */
-
-    /* USER CODE END TIM5_MspInit 0 */
-    /* Peripheral clock enable */
-    __HAL_RCC_TIM5_CLK_ENABLE();
-    /* USER CODE BEGIN TIM5_MspInit 1 */
-
-    /* USER CODE END TIM5_MspInit 1 */
-  }
-
-}
-
-/**
-  * @brief TIM_Base MSP De-Initialization
-  * This function freeze the hardware resources used in this example
-  * @param htim_base: TIM_Base handle pointer
-  * @retval None
-  */
-void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
-{
-  if(htim_base->Instance==TIM4)
-  {
-    /* USER CODE BEGIN TIM4_MspDeInit 0 */
-
-    /* USER CODE END TIM4_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_TIM4_CLK_DISABLE();
-    /* USER CODE BEGIN TIM4_MspDeInit 1 */
-
-    /* USER CODE END TIM4_MspDeInit 1 */
-  }
-  else if(htim_base->Instance==TIM5)
-  {
-    /* USER CODE BEGIN TIM5_MspDeInit 0 */
-
-    /* USER CODE END TIM5_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_TIM5_CLK_DISABLE();
-    /* USER CODE BEGIN TIM5_MspDeInit 1 */
-
-    /* USER CODE END TIM5_MspDeInit 1 */
   }
 
 }
@@ -348,18 +341,16 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     /**USART3 GPIO Configuration
     PB10     ------> USART3_TX
     PB11     ------> USART3_RX
-    PB13     ------> USART3_CTS
-    PB14     ------> USART3_RTS
     */
-    GPIO_InitStruct.Pin = ETH_TX_Pin|ETH_RTS_Pin;
+    GPIO_InitStruct.Pin = ETH_TX_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    HAL_GPIO_Init(ETH_TX_GPIO_Port, &GPIO_InitStruct);
 
-    GPIO_InitStruct.Pin = ETH_RX_Pin|ETH_CTS_Pin;
+    GPIO_InitStruct.Pin = ETH_RX_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+    HAL_GPIO_Init(ETH_RX_GPIO_Port, &GPIO_InitStruct);
 
     /* USER CODE BEGIN USART3_MspInit 1 */
 
@@ -388,10 +379,8 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
     /**USART3 GPIO Configuration
     PB10     ------> USART3_TX
     PB11     ------> USART3_RX
-    PB13     ------> USART3_CTS
-    PB14     ------> USART3_RTS
     */
-    HAL_GPIO_DeInit(GPIOB, ETH_TX_Pin|ETH_RX_Pin|ETH_CTS_Pin|ETH_RTS_Pin);
+    HAL_GPIO_DeInit(GPIOB, ETH_TX_Pin|ETH_RX_Pin);
 
     /* USER CODE BEGIN USART3_MspDeInit 1 */
 
@@ -419,10 +408,10 @@ void HAL_PCD_MspInit(PCD_HandleTypeDef* hpcd)
     /**USB_OTG_FS GPIO Configuration
     PA9     ------> USB_OTG_FS_VBUS
     */
-    GPIO_InitStruct.Pin = USB_VBUS_Pin;
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
     GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(USB_VBUS_GPIO_Port, &GPIO_InitStruct);
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /* Peripheral clock enable */
     __HAL_RCC_USB_OTG_FS_CLK_ENABLE();
@@ -453,7 +442,7 @@ void HAL_PCD_MspDeInit(PCD_HandleTypeDef* hpcd)
     /**USB_OTG_FS GPIO Configuration
     PA9     ------> USB_OTG_FS_VBUS
     */
-    HAL_GPIO_DeInit(USB_VBUS_GPIO_Port, USB_VBUS_Pin);
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_9);
 
     /* USER CODE BEGIN USB_OTG_FS_MspDeInit 1 */
 
